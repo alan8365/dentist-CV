Rotation bug fix

YOLO/util.py
@@ -6,18 +6,19 @@ from glob import glob
 import matplotlib
 import matplotlib.pyplot as plt
 from pathlib import Path
+import numpy as np
 
 
 def crop_by_two_tooth(left, right, margin=50):
     x_items = [left[2], right[0]]
     y_items = [left[1], left[3], right[1], right[3]]
 
-    crop = [
+    crop = np.array([
         min(x_items) - margin,
         min(y_items),
         max(x_items) + margin,
         max(y_items) + margin,
-    ]
+    ])
 
     return crop
 
@@ -51,7 +52,7 @@ def get_teeth_ROI(detected_results, save=False):
                 cls = int(cls.item())
                 name = detected_results.names[cls]
                 teeth_dict[name] = xyxy
-                split_teeth[file_name][name] = {'xyxy': xyxy, 'rotation_offset': [0, 0]}
+                split_teeth[file_name][name] = {'xyxy': xyxy}
 
             teeth_detected_flag = [f in teeth_dict for f in flag_list]
 
@@ -66,7 +67,7 @@ def get_teeth_ROI(detected_results, save=False):
                 left_tooth = teeth_dict[left_tooth_number]
                 right_tooth = teeth_dict[right_tooth_number]
 
-                region = crop_by_two_tooth(left_tooth, right_tooth)
+                region = crop_by_two_tooth(left_tooth, right_tooth).astype(int)
 
                 save_filename = f'{flag}-{number}-{file_name}'
                 save_file = Path(f'./crops/{save_filename}.jpg')
@@ -78,7 +79,7 @@ def get_teeth_ROI(detected_results, save=False):
                     'flag': flag,
                     'number': number,
                     'org_file_name': file_name,
-                    'offset': [region[0], region[1]],
+                    'offset': np.array([region[0], region[1]]),
                     'image': im,
                 }
 
@@ -100,7 +101,7 @@ def get_teeth_ROI(detected_results, save=False):
 if __name__ == "__main__":
     model = torch.hub.load('.', 'custom', path=r'.\weights\8-bound.pt', source='local')
     # Image
-    imgs = glob('../../Datasets/二階段全/*8117*.jpg')
+    imgs = glob('../../Datasets/phase-2/*.jpg')
     # Inference
     results = model(imgs[:10])
 

edge/util.py
@@ -11,7 +11,9 @@ from PIL import Image, ImageOps
 from scipy import ndimage
 from pathlib import Path
 
-from yolov5.utils.plots import save_one_box
+from yolov5.utils.plots import save_one_box, Annotator
+from preprocess.util import recovery_rotated_bounding
+from YOLO.util import get_teeth_ROI
 
 all_tooth_number_dict = {
     'upper': {
@@ -344,20 +346,21 @@ def vertical_separation(source, flag='upper', tooth_type='molar'):
     return window_position, valleys, ver, ver_slope
 
 
-def tooth_isolation(source, flag='upper', tooth_position='left', file_name=None, save=False):
+def tooth_isolation(source, flag='upper', tooth_position='left', filename=None, save=False, rotation_fix=False):
     # Find jaw separation line and gums separation
     # FIXME jaw_spe_line in lower missing
-    crop_images = {}
+    crop_regions = {}
     gum_sep_line, jaw_sep_line, theta, hor_valleys, hor = gum_jaw_separation(source, flag=flag)
+    source_rotated = source.copy()
     if tooth_position == 'middle':
         theta = 0
     else:
-        source = ndimage.rotate(source, theta, reshape=True, cval=255)
+        source_rotated = ndimage.rotate(source, theta, reshape=True, cval=255)
 
     if flag == 'upper':
-        source_roi = source[gum_sep_line:jaw_sep_line, :]
+        source_roi = source_rotated[gum_sep_line:jaw_sep_line, :]
     elif flag == 'lower':
-        source_roi = source[jaw_sep_line:gum_sep_line, :]
+        source_roi = source_rotated[jaw_sep_line:gum_sep_line, :]
     else:
         raise ValueError(f'flag only accept upper or lower but get {flag}.')
 
@@ -370,10 +373,10 @@ def tooth_isolation(source, flag='upper', tooth_position='left', file_name=None,
     tooth_number_dict = all_tooth_number_dict[flag][tooth_position]
     if tooth_position == 'middle':
         if bounding_number < 4:
-            return crop_images
+            return crop_regions
     elif tooth_position == 'left' or tooth_position == 'right':
         if bounding_number < 3:
-            return crop_images
+            return crop_regions
     else:
         raise ValueError(f'tooth_region only accept left, middle or right but get {tooth_position}.')
 
@@ -386,11 +389,13 @@ def tooth_isolation(source, flag='upper', tooth_position='left', file_name=None,
         raise ValueError(f'flag only accept upper or lower but get {flag}.')
 
     unknown_counter = 50
-    source_rgb = cv.cvtColor(source, cv.COLOR_GRAY2RGB)
+    source_rgb = cv.cvtColor(source_rotated, cv.COLOR_GRAY2RGB)
+    bounding_boxes = []
+    tooth_numbers = []
     for i in range(bounding_number):
         x1 = valleys[i]
         x2 = valleys[i + 1]
-        xyxy = torch.Tensor([x1, y1, x2, y2])
+        xyxy = np.array([x1, y1, x2, y2])
 
         try:
             tooth_number = tooth_number_dict[i]
@@ -399,20 +404,86 @@ def tooth_isolation(source, flag='upper', tooth_position='left', file_name=None,
             unknown_counter += 1
 
         if save:
-            save_file = Path(f'./crops/{file_name}-{tooth_number}.jpg')
+            save_file = Path(f'./crops/{filename}/{filename}-{tooth_number}.jpg')
             im = save_one_box(xyxy, source_rgb, save=True, file=save_file)
-            print(f'Tooth crop saved: {file_name}:{tooth_number}')
+            print(f'Tooth crop saved: {filename}:{tooth_number}')
 
         # TODO Rotation offset fix
+        bounding_boxes.append(xyxy)
+        tooth_numbers.append(tooth_number)
+
+    bounding_boxes = np.vstack(bounding_boxes)
+    if rotation_fix:
         theta = math.radians(theta)
-        y1_offset = np.tan(theta) * x1
-        y2_offset = np.tan(theta) * x2
+        shape_h, shape_w = source.shape
+        rotated_xyxy = recovery_rotated_bounding(theta, (shape_w, shape_h), bounding_boxes)
+        bounding_boxes = rotated_xyxy
 
-        region = {tooth_number: {'xyxy': xyxy, 'rotation_offset': [y1_offset, y2_offset]}}
+    for i in range(len(bounding_boxes)):
+        region = {tooth_numbers[i]: {'xyxy': bounding_boxes[i]}}
+        crop_regions.update(region)
 
-        crop_images.update(region)
+    return {'crop_regions': crop_regions, 'angle': theta}
 
-    return crop_images
+
+def bounding_teeth_on_origin(results, save=False, rotation_fix=False):
+    teeth_roi = get_teeth_ROI(results)
+
+    split_teeth = teeth_roi['split_teeth']
+    teeth_roi = teeth_roi['images']
+
+    tooth_position_dict = {
+        0: 'left',
+        1: 'middle',
+        2: 'right'
+    }
+
+    teeth_region = {}
+    for file_name, data in teeth_roi.items():
+        temp_region = {}
+        for datum in data:
+            flag = datum['flag']
+            number = datum['number']
+            im = datum['image']
+            offset = datum['offset']
+
+            tooth_position = tooth_position_dict[number]
+            im_g = cv.cvtColor(im, cv.COLOR_RGB2GRAY)
+
+            isolation_data = tooth_isolation(im_g, flag=flag, filename=file_name, tooth_position=tooth_position,
+                                             save=save,
+                                             rotation_fix=rotation_fix)
+            region = isolation_data['crop_regions']
+            for k, v in region.items():
+                xyxy = region[k]['xyxy']
+
+                xyxy[0] += offset[0]
+                xyxy[1] += offset[1]
+                xyxy[2] += offset[0]
+                xyxy[3] += offset[1]
+
+                region[k]['xyxy'] = xyxy
+
+            temp_region.update(region)
+            temp_region.update(split_teeth[file_name])
+
+            teeth_region[file_name] = temp_region
+
+    # Display bounding area
+    for file_name, data in teeth_region.items():
+        im0 = cv.imread(f'../Datasets/phase-2/{file_name}.jpg')
+        annotator = Annotator(im0, line_width=3, example=file_name)
+        for tooth_number, body in data.items():
+            xyxy = body['xyxy']
+            annotator.box_label(xyxy, str(tooth_number), color=(255, 0, 0))
+
+        im1 = annotator.result()
+
+        plt.imshow(im1)
+        plt.title(file_name)
+        plt.show()
+
+    return teeth_region
 
 
 if __name__ == '__main__':

preprocess/util.py
@@ -1,10 +1,45 @@
 import os
 import json
 
-import pandas as pd
 import torch
+import numpy as np
+import pandas as pd
 
 from glob import glob
+from math import sin, cos
+
+
+def xyxy_reformat(x):
+    # Convert nx4 boxes to xy1=top-left, xy2=bottom-right
+    # y = x.clone() if isinstance(x, torch.Tensor) else np.copy(x)
+    y = np.copy(x)
+    y[:, 0] = x[:, [0, 2]].min(axis=1)  # right point x
+    y[:, 1] = x[:, [1, 3]].min(axis=1)  # top point y
+    y[:, 2] = x[:, [0, 2]].max(axis=1)  # left point x
+    y[:, 3] = x[:, [1, 3]].max(axis=1)  # bottom point y
+    return y
+
+
+def xyxy2xywh(x):
+    # Convert nx4 boxes from [x1, y1, x2, y2] to [x, y, w, h] where xy1=top-left, xy2=bottom-right
+    # y = x.clone() if isinstance(x, torch.Tensor) else np.copy(x)
+    y = np.copy(x)
+    y[:, 0] = (x[:, 0] + x[:, 2]) / 2  # x center
+    y[:, 1] = (x[:, 1] + x[:, 3]) / 2  # y center
+    y[:, 2] = x[:, 2] - x[:, 0]  # width
+    y[:, 3] = x[:, 3] - x[:, 1]  # height
+    return y
+
+
+def xywh2xyxy(x):
+    # Convert nx4 boxes from [x, y, w, h] to [x1, y1, x2, y2] where xy1=top-left, xy2=bottom-right
+    # y = x.clone() if isinstance(x, torch.Tensor) else np.copy(x)
+    y = np.copy(x)
+    y[:, 0] = x[:, 0] - x[:, 2] / 2  # top left x
+    y[:, 1] = x[:, 1] - x[:, 3] / 2  # top left y
+    y[:, 2] = x[:, 0] + x[:, 2] / 2  # bottom right x
+    y[:, 3] = x[:, 1] + x[:, 3] / 2  # bottom right y
+    return y
 
 
 def rect_include_another(box1, box2, eps=1e-9):
@@ -67,8 +102,8 @@ def get_image_by_labels(target_labels, file_name='label_TF.csv'):
     return df[result_mask]
 
 
-def get_labels_by_image(file_path, target_labels=None):
-    with open(file_path, 'r', encoding='utf-8') as f:
+def get_labels_by_image(filepath, target_labels=None):
+    with open(filepath, 'r', encoding='utf-8') as f:
         data = json.load(f)
         shapes = data['shapes']
 
@@ -111,6 +146,64 @@ def get_classification_format(teeth_region, target_labels, dataset_dir=None):
                     result.append({f'{json_filename}-{key}': label})
 
 
+def recovery_rotated_bounding(theta, org_image_shape, bounding_boxes):
+    rot_matrix = np.copy([[cos(theta), -sin(theta)], [sin(theta), cos(theta)]])
+    in_center, out_center = get_rotation_center(org_image_shape, rot_matrix)
+    offset = in_center - out_center
+
+    # Compute rotation
+    xywh_rot_matrix = np.array([[cos(theta), -sin(theta), 0, 0],
+                                [sin(theta), cos(theta), 0, 0],
+                                [0, 0, cos(theta), sin(abs(theta))],
+                                [0, 0, sin(abs(theta)), cos(theta)], ])
+    bounding_boxes_rotated = np.array(bounding_boxes)
+    bounding_boxes_rotated = xyxy2xywh(bounding_boxes_rotated)
+    bounding_boxes_rotated = bounding_boxes_rotated.transpose()
+
+    bounding_boxes_rotated = xywh_rot_matrix @ bounding_boxes_rotated
+
+    margin = np.array([offset[0], offset[1], 0, 0])
+    bounding_boxes_rotated = bounding_boxes_rotated + margin[:, None]
+    print(bounding_boxes_rotated)
+    bounding_boxes_rotated = xywh2xyxy(bounding_boxes_rotated.transpose()).astype(int)
+
+    return bounding_boxes_rotated
+
+
+def get_rotation_center(org_image_shape, rot_matrix):
+    in_plane_shape = np.array(org_image_shape)
+    # Compute transformed input bounds
+    iy, ix = in_plane_shape
+    out_bounds = rot_matrix @ [[0, 0, iy, iy],
+                               [0, ix, 0, ix]]
+    # Compute the shape of the transformed input plane
+    out_plane_shape = (out_bounds.ptp(axis=1) + 0.5).astype(int)
+    out_center = rot_matrix @ ((out_plane_shape - 1) / 2)
+    in_center = (in_plane_shape - 1) / 2
+    return in_center, out_center
+
+
+def rotate_bounding_boxes(theta, org_image_shape, bounding_boxes):
+    rot_matrix = np.copy([[cos(theta), -sin(theta)], [sin(theta), cos(theta)]])
+    in_center, out_center = get_rotation_center(org_image_shape, rot_matrix)
+    offset = out_center - in_center
+    offset = np.tile(offset, 2)
+
+    rot_matrix = np.copy([[cos(-theta), -sin(-theta)], [sin(-theta), cos(-theta)]])
+    bounding_boxes_rotated = np.array(bounding_boxes)
+    boxes_shape = bounding_boxes_rotated.shape
+
+    bounding_boxes_rotated = bounding_boxes_rotated + np.tile(offset, boxes_shape[0])
+    bounding_boxes_rotated = bounding_boxes_rotated.reshape((boxes_shape[0], 2, 2), order='F')
+    bounding_boxes_rotated = (rot_matrix @ bounding_boxes_rotated).reshape((boxes_shape[0], 4), order='F')
+
+    return bounding_boxes_rotated
+
+
 # TODO classified by tooth number
 if __name__ == '__main__':
-    pass
+    phi = 0.3490658503988659
+    shape = [326, 443]
+    xyxy = [[144.91, 34.606, 276.73, 136.12]]
+
+    xyxy = rotate_bounding_boxes(phi, shape, xyxy)